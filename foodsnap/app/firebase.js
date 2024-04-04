a10a0808c01696db0e21506f0d399e809cb6c6da
Esub Shaik <esubshaik7864@gmail.com>
2024-04-04 22:44:24 +0530

    Added Licence

---
+++ foodsnap/app/firebase.js
@@ -0,0 +1,15 @@
+// import { initializeApp } from 'firebase/app';
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyASqe229HSypauvAW8KI7hjiJZCe_JB9EY',
+// //   authDomain: '<your-auth-domain>',
+//   projectId: 'foodsnap-1456',
+//   storageBucket: 'foodsnap-1456.appspot.com',
+//   messagingSenderId: '562723776196',
+//   appId: '1:562723776196:android:097cbb4ea7b495f3490b91',
+// };
+
+// const app = initializeApp(firebaseConfig);
+// console.log(app);
+
+// export default app;
